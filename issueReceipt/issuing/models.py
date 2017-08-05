8ab99fdfd21075f97bd590597cac2df4d87c9888
NaoY-2501 <solaris2501.fe2@gmail.com>
2017-08-05 15:22:43 +0900

    Prototype(without receipt page)

--- issueReceipt/issuing/models.py
+++ issueReceipt/issuing/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Attendee(models.Model):
+    register_id = models.CharField(max_length=10)
+    register_type = models.CharField(max_length=8)
+
+    def __str__(self):
+        return self.register_id
--- issueReceipt/issuing/views.py
+++ issueReceipt/issuing/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Attendee
+from .forms import AttendeeForm
+from datetime import datetime
 
-# Create your views here.
+def issuing(request):
+    today = datetime.now()
+    issue_date = "{}年{}月{}日".format(str(today.year),str(today.month).lstrip('0'),str(today.day).lstrip('0'))
+    print(issue_date)
+    if request.method == 'POST':
+        form = AttendeeForm(request.POST)
+        if form.is_valid():
+            try:
+                model = Attendee.objects.get(register_id=form.cleaned_data['register_id'])
+                form.register_type = model.register_type
+                form.issue_date = issue_date
+                return render(request,'issuing/receipt.html',{'form':form})
+            except:
+                form.error = '受付番号が見つかりませんでした。'
+                return render(request,'issuing/issuing.html',{'form':form})
+    else:
+        form = AttendeeForm()
+    return render(request,'issuing/issuing.html',{'form':form })
