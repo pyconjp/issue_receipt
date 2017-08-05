f47d7d01692bb7e8d558582fa2bb2c03102df640
NaoY-2501 <solaris2501.fe2@gmail.com>
2017-08-05 17:34:59 +0900

    Prototype with receipt

--- issueReceipt/issuing/views.py
+++ issueReceipt/issuing/views.py
@@ -6,14 +6,19 @@ from datetime import datetime
 def issuing(request):
     today = datetime.now()
     issue_date = "{}年{}月{}日".format(str(today.year),str(today.month).lstrip('0'),str(today.day).lstrip('0'))
-    print(issue_date)
+    pycon = "PyCon JP {} ".format(str(today.year))
     if request.method == 'POST':
         form = AttendeeForm(request.POST)
         if form.is_valid():
             try:
                 model = Attendee.objects.get(register_id=form.cleaned_data['register_id'])
                 form.register_type = model.register_type
+                if model.register_type == "Business":
+                    form.price = "15,000"
+                elif model.register_type == "Patron":
+                    form.price = "40,000"
                 form.issue_date = issue_date
+                form.pycon = pycon
                 return render(request,'issuing/receipt.html',{'form':form})
             except:
                 form.error = '受付番号が見つかりませんでした。'
