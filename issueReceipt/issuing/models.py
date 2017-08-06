9ebd8bf09751095f4a8ac0e47b16c38116fe7856
NaoY-2501 <solaris2501.fe2@gmail.com>
2017-08-06 16:40:04 +0900

    Re-migrate and add stamp to receipt.

--- issueReceipt/issuing/models.py
+++ issueReceipt/issuing/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Attendee(models.Model):
     register_id = models.CharField(max_length=6)
-    register_name = models.CharField(max_length=50)
+    register_name = models.CharField(max_length=50,default=' ')
     register_type = models.CharField(max_length=8)
 
     def __str__(self):
--- issueReceipt/issuing/views.py
+++ issueReceipt/issuing/views.py
@@ -4,14 +4,17 @@ from .forms import AttendeeForm
 from datetime import datetime
 
 def issuing(request):
+
     today = datetime.now()
+    # 発行日
     issue_date = "{}年{}月{}日".format(str(today.year),str(today.month).lstrip('0'),str(today.day).lstrip('0'))
+    # 但し書き用 PyCon JP yyyy
     pycon = "PyCon JP {} ".format(str(today.year))
     if request.method == 'POST':
         form = AttendeeForm(request.POST)
         if form.is_valid():
             try:
-                model = Attendee.objects.get(register_id=form.cleaned_data['register_id'])
+                model = Attendee.objects.get(register_id=form.cleaned_data['register_id'],register_name=form.cleaned_data['register_name'])
                 form.register_type = model.register_type
                 if model.register_type == "Business":
                     form.price = "15,000"
@@ -21,7 +24,7 @@ def issuing(request):
                 form.pycon = pycon
                 return render(request,'issuing/receipt.html',{'form':form})
             except:
-                form.error = '受付番号が見つかりませんでした。'
+                form.error = '入力された受付番号・connpass IDに該当する領収書が見つかりませんでした。'
                 return render(request,'issuing/issuing.html',{'form':form})
     else:
         form = AttendeeForm()
